d02f7ce7a81d25f190f5e9b1c161494859f272f3
Leonardo Lavigne <devleolavigne@gmail.com>
2020-05-10 17:16:56 -0300

    add functions

---
+++ script.js
@@ -0,0 +1,32 @@
+const MAX_VISOR_CHAR = 8
+let history = []
+
+function addNum(param) {
+  if (document.getElementById('visor').innerHTML.length < MAX_VISOR_CHAR) {
+    document.getElementById('visor').innerHTML += param
+  }
+}
+
+function calcAction(action) {
+  let currentChar = document.getElementById('visor').innerHTML
+  if (currentChar.length === 0) { return }
+  history.push(Number(document.getElementById('visor').innerHTML))
+  document.getElementById('history').innerHTML += `${document.getElementById('visor').innerHTML} ${action} `
+  document.getElementById('visor').innerHTML = ''
+  history.push(action)
+}
+
+function CleanCurrentEntry() {
+  document.getElementById('visor').innerHTML = ''
+}
+
+function cleanAll() {
+  document.getElementById('visor').innerHTML = ''
+  document.getElementById('history').innerHTML = ''
+  history = []
+}
+function equal() {
+  let currentNumber = document.getElementById('visor').innerHTML
+  let result = document.getElementById('history').innerHTML + currentNumber
+  result.map()
+}
